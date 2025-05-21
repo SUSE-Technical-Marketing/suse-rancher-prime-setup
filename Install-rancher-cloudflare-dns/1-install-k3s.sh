1f9bf6a31f57d73f8818c70311493cccc89c93b8
wiredquill <erin@suse.com>
2025-05-21 08:53:24 -0700

    Update 1-install-k3s.sh

--- Install-rancher-cloudflare-dns/1-install-k3s.sh
+++ Install-rancher-cloudflare-dns/1-install-k3s.sh
@@ -4,10 +4,17 @@ set -e
 # Install K3s 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.31.6+k3s1" sh -
 
-# Copy kubeconfig and make it readable
+# Wait for kubeconfig to become available
+CONFIG_FILE="/etc/rancher/k3s/k3s.yaml"
+echo "Waiting for kubeconfig file to be created..."
+while [ ! -f "$CONFIG_FILE" ]; do
+  sleep 2
+done
 
-mkdir ~/.kube
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+# Copy kubeconfig and make it readable
+mkdir -p ~/.kube
+cp "$CONFIG_FILE" ~/.kube/config
 chmod 600 ~/.kube/config
 
+echo "K3s is installed. Verifying nodes..."
 kubectl get nodes
