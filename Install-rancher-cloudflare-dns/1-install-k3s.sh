8cc3b5e3da11464bc5ddf75718084e64861a1bf1
wiredquill <erin@suse.com>
2025-05-21 08:37:24 -0700

    Update 1-install-k3s.sh

--- Install-rancher-cloudflare-dns/1-install-k3s.sh
+++ Install-rancher-cloudflare-dns/1-install-k3s.sh
@@ -4,5 +4,10 @@ set -e
 # Install K3s 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.31.6+k3s1" sh -
 
-# Make the kubeconfig readable for erin
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+# Copy kubeconfig and make it readable
+
+mkdir ~./kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+chmod 600 ~/.kube/config
+
+kubectl get nodes
