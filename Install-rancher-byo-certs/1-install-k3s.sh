bdd41a9ffb5989db34144f99beb2a211e52b1f24
wiredquill <erin@suse.com>
2025-10-25 14:03:20 -0700

    Update K3s installation version to v1.32.8

--- Install-rancher-byo-certs/1-install-k3s.sh
+++ Install-rancher-byo-certs/1-install-k3s.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install K3s without Traefik
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.31.6+k3s1" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.32.8+k3s1" sh -
 
 # Make the kubeconfig readable for erin
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
